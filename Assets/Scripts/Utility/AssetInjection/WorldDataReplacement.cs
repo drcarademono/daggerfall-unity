2af7e31f98bb598c1b1574fa570237456924cf56
drcarademono <73616371+drcarademono@users.noreply.github.com>
2025-06-11 11:43:02 -0700

    Moved fallback up

--- Assets/Scripts/Utility/AssetInjection/WorldDataReplacement.cs
+++ Assets/Scripts/Utility/AssetInjection/WorldDataReplacement.cs
@@ -568,8 +568,10 @@ namespace DaggerfallWorkshop.Utility.AssetInjection
             // Check for the "Index" field and assign its value
             jsonBlockIndex = dfBlock.Value.Index;
 
-            // If jsonBlockIndex is invalid (less than or equal to nextBlockIndex), use fallback method
-            if (jsonBlockIndex <= nextBlockIndex)
+            // If jsonBlockIndex is invalid (less than or equal to nextBlockIndex) or index is already taken, use fallback method
+            if (jsonBlockIndex <= nextBlockIndex
+                || newBlockNames.ContainsKey(jsonBlockIndex)
+                || newBlockIndices.ContainsKey(blockName))
             {
                 int newIndex = nextBlockIndex;
 
@@ -586,17 +588,9 @@ namespace DaggerfallWorkshop.Utility.AssetInjection
                 return;
             }
 
-            // Add to cache, but if that jsonBlockIndex or blockName is already taken, fallback again
-            if (newBlockNames.ContainsKey(jsonBlockIndex) || newBlockIndices.ContainsKey(blockName))
-            {
-                AssignNextIndex(blockName);
-            }
-            else
-            {
-                newBlockNames[jsonBlockIndex] = blockName;
-                newBlockIndices[blockName] = jsonBlockIndex;
-            }
-
+            // Add to cache
+            newBlockNames[jsonBlockIndex] = blockName;
+            newBlockIndices[blockName] = jsonBlockIndex;
             Debug.LogFormat("Found a new DFBlock: {0}, (assigned index: {1})", blockName, jsonBlockIndex);
 
             // Cache the full DFBlock
